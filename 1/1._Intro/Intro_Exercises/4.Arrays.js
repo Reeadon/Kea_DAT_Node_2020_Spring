ddf792ab986414752f7eb61ba43a08051ba7703e
anders <anderslatif@gmail.com>
2020-02-06 14:04:48 +0100

    Created our first API

--- 1/1._Intro/Intro_Exercises/4.Arrays.js
+++ 1/1._Intro/Intro_Exercises/4.Arrays.js
@@ -69,9 +69,23 @@ diet.pop();
 
 // You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.  
 
+var dinnerTray = diet.slice();
+
+dinnerTray[0] = "hello";
+
+var spreadDinner = [...diet];
+
+console.log(...diet);
+
+var cocaine = {weight: 10};
+
+var moreCocaine = {...cocaine, color: "white"};
+
+console.log(moreCocaine, cocaine);
+
 // --------------------------------------
 
- array.slice(start, end)  // start and end are optional
+// array.slice(start, end)  // start and end are optional
 
 // --------------------------------------
 // Exercise 7 - For loop
@@ -80,12 +94,29 @@ var letters = ["a","b","c", "d", "e", "f", "g", "h"];
 
 // log every second char in the array starting from b
 
+for (var i = 1; i < letters.length; i += 2) {
+    console.log(letters[i]);
+}
+
+
 // --------------------------------------
 // Exercise 8 - For loop and if statement
 
 var numbers  = [5, 3, 2, 7, 11, 12, 0, -20, 6];
 
 var discardedNumbers = [];
+
+// enhanced for loop
+for (e of numbers) {
+    if (e < 0 || e > 6) {
+        console.log(e);
+    }
+    else {
+        discardedNumbers.push(e);
+    }
+}
+
+console.log(discardedNumbers);
 
 // log the element if the number is above 6 or below 0
 // else push them to the array discardedNumbers
