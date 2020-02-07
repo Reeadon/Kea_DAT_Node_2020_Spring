64428e2edd51faf3f7fb484e0e3c47be76893e92
anders <anderslatif@gmail.com>
2020-02-07 11:10:20 +0100

    Added more string exercises

--- 2/1._Intro_Files/1._Exercises/1._Basics.js
+++ 2/1._Intro_Files/1._Exercises/1._Basics.js
@@ -9,14 +9,28 @@ var lastName = "Latif";
 // show in the console
 // My first name is Anders and my last name is Latif
 
+console.log("My first name is", firstName, "my last name is", lastName);
+
+
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
 
-var year = "2019";
+var year = "2019abc123";
 var number = 1;
 
 // Add the year plus the number
 // The result should be 2020
 // You cannot touch line 1 or 2
 
+//var newYear = parseInt(year) + number;
+
+console.log(parseInt(year));
+
+// var newYear = Number(year) + number;
+
+// console.log(newYear);
+
+// console.log(+year + number);
+
 // --------------------------------------
+
