2137b3de571d8f827f6fd1354bc9f8461b170712
anders <anderslatif@gmail.com>
2020-02-07 13:18:02 +0100

    Added Array and loop exercises

--- 2/1._Intro_Files/1._Exercises/3._Objects.js
+++ 2/1._Intro_Files/1._Exercises/3._Objects.js
@@ -7,6 +7,14 @@ var myObj = {"message": "Hello, earthling! I bring peace."};
 
 // Log the message 
 
+// dot notation
+console.log(myObj.message);
+
+// bracket notation / square notation
+console.log(myObj["message"]);
+
+
+
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
@@ -19,12 +27,23 @@ var stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
 
+stackOverflow.isAllowed = true;
+
 // --------------------------------------
 // Exercise 4 - Remove a property 
 
 var thisSong = {"description": "The best song in the world."}
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
+
+delete thisSong.description;
+
+// thisSong['about'] = "Just a tribute";
+
+thisSong.about = "Just a tribute";
+
+console.log(thisSong);
+
 
 // --------------------------------------
 
