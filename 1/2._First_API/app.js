1448cfa116e5173fded56ef68ad2d8af0318f522
anders <anderslatif@gmail.com>
2020-02-13 13:00:50 +0100

    Taught about functions in Javascript and worked more with Express

--- 1/2._First_API/app.js
+++ 1/2._First_API/app.js
@@ -1,5 +1,38 @@
-var express = require("express");
-var app = express();
+const app = require("express")(); 
 
 
-app.listen(3000);
+app.get("/", (req, res) => {
+    res.send({ message: "Hello there" });
+});
+
+// define something on the path /aboutMe that returns a JSON representation of you
+
+app.get("/aboutMe", (req, res) => {
+    const me = {
+        name: "Radu"
+    };
+    res.send(me);
+});
+
+// define something on the path /aboutThisWebsite 
+// that returns a JSON representation of the website
+app.get("/aboutThisWebsite", (req, res) => {
+    const aboutThisWebsite = {
+        name: "thiswebsiteIdunno"
+    };
+    //if (aboutThisWebsite) {
+        res.send(aboutThisWebsite);
+    // }
+    // res.send("Sorry, no info about this website.");
+    // this here won't run
+});
+
+
+
+
+app.listen(3000, error => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server is running on port", 3000);
+});
