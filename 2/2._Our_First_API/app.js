b57b51550db470a981a4b2340be5e1fc137a6f58
anders <anderslatif@gmail.com>
2020-02-14 14:00:16 +0100

    Worked on our first API

--- 2/2._Our_First_API/app.js
+++ 2/2._Our_First_API/app.js
@@ -1,8 +1,35 @@
-/* var express = require("express");
-var app = express();
+/* const express = require("express");
+const app = express();
  */
-var app = require("express")();
+const app = require("express")();
+
+            // callback function 
+app.get("/", (req, res) => {
+    const response = {
+        message: "hi there"
+    }
+    res.send(response);
+});
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", 
+                "Thursday", "Friday", "Saturday"];
+
+app.get("/time", (req, res) => {
+    const date = new Date();
+    res.send({ 
+        unformatedTime: date,
+        time: date.toString(),
+        day: date.getDay(),
+        weekDay: days[date.getDay()]
+    });
+});
+
+// days of the week assignment
 
 
-
-app.listen(3000);
+app.listen(3000, error => {
+    if (error) {
+        console.log("Error running the server", error);
+    }
+    console.log("Server is running on port", 3000);
+});
