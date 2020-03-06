b8dddc808e0a37f2e604556551a98b5de7d834fe
anders <anderslatif@gmail.com>
2020-03-06 14:00:02 +0100

    Made the player.html page dynamically accept the video id

--- 2/5._Keatube/public/player/player.js
+++ 2/5._Keatube/public/player/player.js
@@ -1,8 +1,12 @@
 const url = window.location.href;
 let videoId = url.substr(url.lastIndexOf("/") + 1);
 
-console.log(" video id ", videoId);
+const player = `<video controls>
+                    <source src="/${videoId}" >
+                    Your browser does not support the video tag.
+                </video>`;
 
+$("#player").append(player);
 
 
 
