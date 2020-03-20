f2d1929596a931440765616ff0396092de1261a7
anders <anderslatif@gmail.com>
2020-03-20 13:51:25 +0100

    Got the data in the frontpage and showing it with jQuery

--- 2/5._Keatube/routes/videos.js
+++ 2/5._Keatube/routes/videos.js
@@ -1,10 +1,23 @@
 const router = require("express").Router();
+const uuid4 = require("uuid").v4; 
 
-// what data do we want in our videos array? {}, ok but what keys and values?
-const videos = [];
 
-// Implement below... what should it do?
-// get /videos 
+const videos = [{ 
+                id: "", 
+                title: "NightSky",
+                thumbnail: "", 
+                description: "Watch this beautiful sky and enjoy the stars",
+                path: "/29a4415c-81c3-45ca-b95f-fe18c74998a0.mp4",
+                uploadDate: "", 
+                category: "Science", 
+                tags: ["stars", "sky"] 
+            }];
+
+router.get("/videos", (req, res) => {
+    return res.send({ response: videos });
+});
+
+
 
 
 module.exports = router;
