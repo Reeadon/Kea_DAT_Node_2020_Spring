2210ef846eb2e3d15b05b1dec8036db549a0571f
anders <anderslatif@gmail.com>
2020-03-26 09:58:54 +0100

    Implemented Ajax in the client so that the videos are dynamically loaded in the frontpage

--- 1/5._Keatube/public/frontpage/frontpage.js
+++ 1/5._Keatube/public/frontpage/frontpage.js
@@ -1,2 +1,8 @@
-
+$.get("videos", (response) => {
+    console.log(response.response);
+    response.response.map((video) => {
+        $("#video-gallery")
+        .append(`<a href="/player/${video.fileName}">${video.title}</a>`);
+    });
+});
 
--- 1/5._Keatube/routes/videos.js
+++ 1/5._Keatube/routes/videos.js
@@ -1,10 +1,22 @@
 const router = require("express").Router();
+const uuid = require('uuid').v4;
 
-// todo what data should be inside of each video object
-const videos = [];
+const videos = [{
+    title: "Ocean Waves",
+    description: "Watch the waves and enjoy",
+    fileName: "3f1e146c-3295-444a-a7be-e758c560536b.mp4",
+    thumbnail: "",
+    category: "Nature",
+    tags: ["waves", "ocean", "coast"],
+    uploadDate: new Date(2020, 3, 26, 08, 43)
+}];
 
-router.get("/test", (req, res) => {
-    return res.send({ message: "Does the router work?" });
+router.get("/videos", (req, res) => {
+    return res.send({ response: videos });
+});
+
+router.get("/videos/:videoId", (req, res) => {       
+    return res.send({ response: videos.find(video => video.fileName === req.params.videoId) });
 });
 
 module.exports = router;
