83c5f278564216d2242188461a45fe7d820724e0
anders <anderslatif@gmail.com>
2020-03-27 09:12:10 +0100

    Implemented the sticky footer

--- 2/5._Keatube/public/player/player.js
+++ 2/5._Keatube/public/player/player.js
@@ -1,7 +1,9 @@
 const url = window.location.href;
 let videoId = url.substr(url.lastIndexOf("/") + 1);
 
-const player = `<video controls>
+// todo: Use Ajax to get the json info about this specific video.
+
+const player = `<video id="player" controls>
                     <source src="/${videoId}" >
                     Your browser does not support the video tag.
                 </video>`;
