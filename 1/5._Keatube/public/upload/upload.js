7fd51fae830cfba20dd9dc2203507fd70584618e
anders <anderslatif@gmail.com>
2020-04-02 08:57:11 +0200

    Started doing client-side validation

--- 1/5._Keatube/public/upload/upload.js
+++ 1/5._Keatube/public/upload/upload.js
@@ -1,3 +1,36 @@
+let fileValid = false;
+
 function validateForm() {
-    return true;
+    const title = document.forms.videoupload.title.value;
+    const description = document.forms.videoupload.description.value;
+    
+    if (title < 8 || title > 64) {
+        return false;
+    }
+
+    if (description > 2048) {
+        return false;
+    }
+
+    return true && fileValid;
 }
+
+function handleFileUpload(files) {
+    const file = files[0];
+    
+    const mimeTypeArray = file.type.split("/");
+
+    if (mimeTypeArray[0] !== "video") {
+        fileValid = false;
+        return;
+    }
+
+    const fileSize = file.size;
+
+    if (fileSize > xxxx) {
+        fileValid = false;
+        return;
+    }
+
+}
+
