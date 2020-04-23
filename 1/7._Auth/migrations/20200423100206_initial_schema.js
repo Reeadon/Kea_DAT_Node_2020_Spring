66f68b492c3dbacde043a2af643eb52b6c45aa70
anders <anderslatif@gmail.com>
2020-04-23 13:17:17 +0200

    Finished the migration file and created the first roles seed

--- 1/7._Auth/migrations/20200423100206_initial_schema.js
+++ 1/7._Auth/migrations/20200423100206_initial_schema.js
@@ -1,17 +1,29 @@
+// DDL = DEFINE = CREATE, DROP (this migrations file)
+// DML = MANIPULATE = SELECT, UPDATE, DELETE (seeds)
 
 exports.up = function(knex) {
+
     return knex.schema
+        .createTable('roles', (table) => {
+            table.increments('id').notNullable();
+            table.string('role').unique().notNullable();
+        })
         .createTable('users', (table) => {
-            table.increments('id');
+            table.increments('id').notNullable();
             table.string('username').unique().notNullable();
             table.string('password').notNullable();
             table.integer('age');
-            table.timestamp('updated_at');
-            table.timestamp('created_at').defaultTo(knex.fn.now());
+
+            table.integer('role_id').unsigned().notNullable();
+            table.foreign('role_id').references('roles.id');
+
+            table.dateTime('updated_at').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            table.dateTime('created_at').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP'));
         });
 };
-// // todo ROLE
 
 exports.down = function(knex) {
- // rollback
+    return knex.schema
+        .dropTableIfExists('users')
+        .dropTableIfExists('roles');
 };
