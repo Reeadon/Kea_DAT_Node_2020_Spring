852e5e22241e90b149e21e44d8c1eacad2b91a57
anders <anderslatif@gmail.com>
2020-05-01 11:15:26 +0200

    Implemented finding user based on username

--- 2/7._Auth/routes/auth.js
+++ 2/7._Auth/routes/auth.js
@@ -13,10 +13,29 @@ router.post('/signup', (req, res) => {
     }); */
     const { username, password } = req.body;
 
+    if (username && password) {
+        // password validation
+        if (password.length < 8) {
+            return res.status(400).send({ response: "Password must be 8 characters or longer" });
+        } else {
+            try {
+                User.query().select('username').where('username', username).then(foundUser => {
+                    if (foundUser.length > 0) {
+                        return res.status(400).send({ response: "User already exists" });
+                    } else {
+                        // 2. add a new user
+
+                    }
+                    return res.send({ response: foundUser });
     
-
-    return res.send({ response: req.body });
-
+                });
+            } catch (error) {
+                return res.status(500).send({ response: "Something went wrong with the DB" });
+            }
+        }
+    } else {
+        return res.status(400).send({ response: "username or password missing" });
+    }
 });
 
 router.get('/logout', (req, res) => {
