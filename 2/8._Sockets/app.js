f4ed3f7b754e09f9f996d7ea0c1f0fb6f9e41064
anders <anderslatif@gmail.com>
2020-05-15 13:46:25 +0200

    Added security to the sockets project

--- 2/8._Sockets/app.js
+++ 2/8._Sockets/app.js
@@ -4,8 +4,26 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
+const helmet = require('helmet');
+app.use(helmet());
+
+const escape = require('escape-html');
+
 io.on('connection', socket => {
-    console.log("hello?????", socket.id);
+    // console.log(socket.id);
+
+    socket.on('a client wrote this', (data) => {
+        // emits to all clients
+        io.emit("A client said", { thoughts: escape(data.thoughts) });
+
+        // only emits to the very socket that fired the event
+        // socket.emit("A client said", data);
+
+        // emits to all except the socket itself
+        // socket.broadcast.emit("A client said", data);
+    });
+
+
 });
 
 app.get("/", (req, res) => {
